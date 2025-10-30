96c1ba6201373543bc89fa8cc09a35017c271fce
marcin <marcin7848@gmail.com>
2025-10-30 22:05:01 +0100

    fix entity

--- src/main/java/io/learn/lexigeek/language/domain/Account.java
+++ src/main/java/io/learn/lexigeek/language/domain/Account.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
+@Entity(name = "LanguageAccount")
 @Table(name = "accounts")
 @EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
 @Getter
