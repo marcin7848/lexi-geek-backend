eb8dc8ec714e59227364e4c1fa4f876747ccff1b
marcin <marcin7848@gmail.com>
2025-12-06 23:53:22 +0100

    fixes

--- src/main/java/io/learn/lexigeek/word/dto/CheckAnswerForm.java
+++ src/main/java/io/learn/lexigeek/word/dto/CheckAnswerForm.java
@@ -1,8 +1,10 @@
 package io.learn.lexigeek.word.dto;
 
+import io.learn.lexigeek.word.domain.WordMethod;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.Map;
 
-public record CheckAnswerForm(@NotNull Map<String, String> answers) {
+public record CheckAnswerForm(@NotNull Map<String, String> answers,
+                              @NotNull WordMethod method) {
 }
