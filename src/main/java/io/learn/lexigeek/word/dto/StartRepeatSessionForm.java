d9420413e2099aaca33f244022c330139e71b4f6
marcin <marcin7848@gmail.com>
2025-12-06 16:34:39 +0100

    add includeChosen

--- src/main/java/io/learn/lexigeek/word/dto/StartRepeatSessionForm.java
+++ src/main/java/io/learn/lexigeek/word/dto/StartRepeatSessionForm.java
@@ -10,5 +10,6 @@ import java.util.UUID;
 public record StartRepeatSessionForm(@NotEmpty List<UUID> categoryUuids,
                                      @NotNull
                                      @Min(value = 1) Integer wordCount,
-                                     @NotNull RepeatMethod method) {
+                                     @NotNull RepeatMethod method,
+                                     @NotNull Boolean includeChosen) {
 }
