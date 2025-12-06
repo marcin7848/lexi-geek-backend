62d25b1a49c5b8e1feb367ddff4296d6f447c7eb
marcin <marcin7848@gmail.com>
2025-12-06 18:17:04 +0100

    fixes

--- src/main/java/io/learn/lexigeek/word/dto/RepeatWordDto.java
+++ src/main/java/io/learn/lexigeek/word/dto/RepeatWordDto.java
@@ -7,11 +7,10 @@ import io.learn.lexigeek.word.domain.WordMethod;
 import java.util.List;
 import java.util.UUID;
 
-public record RepeatWordDto(
-        UUID uuid,
-        String comment,
-        WordMechanism mechanism,
-        List<WordPartDto> wordParts,
-        WordMethod method,
-        CategoryMode categoryMode) {
+public record RepeatWordDto(UUID uuid,
+                            String comment,
+                            WordMechanism mechanism,
+                            List<WordPartDto> wordParts,
+                            WordMethod method,
+                            CategoryMode categoryMode) {
 }
