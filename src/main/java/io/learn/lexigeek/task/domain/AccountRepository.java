fe353386f4fc43a33ed18ba4da00b0264ab0dcfd
marcin <marcin7848@gmail.com>
2025-12-14 22:08:12 +0100

    fix tasks

--- src/main/java/io/learn/lexigeek/task/domain/AccountRepository.java
+++ src/main/java/io/learn/lexigeek/task/domain/AccountRepository.java
@@ -3,6 +3,6 @@ package io.learn.lexigeek.task.domain;
 import io.learn.lexigeek.common.repository.UUIDAwareJpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository("TaskAccountRepository")
 interface AccountRepository extends UUIDAwareJpaRepository<Account, Long> {
 }
