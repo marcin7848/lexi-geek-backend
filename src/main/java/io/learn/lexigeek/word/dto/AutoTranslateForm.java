2c49451b14144192ca47cd0d08f5b0cefab4753e
marcin <marcin7848@gmail.com>
2025-12-16 21:33:35 +0100

    add source and traget translation language

--- src/main/java/io/learn/lexigeek/word/dto/AutoTranslateForm.java
+++ src/main/java/io/learn/lexigeek/word/dto/AutoTranslateForm.java
@@ -1,9 +1,9 @@
 package io.learn.lexigeek.word.dto;
 
-import io.learn.lexigeek.word.domain.AutomaticTranslationMethod;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
-public record AutoTranslateForm(@NotNull AutomaticTranslationMethod method,
+public record AutoTranslateForm(@NotNull @NotEmpty String sourceLanguage,
+                                @NotNull @NotEmpty String targetLanguage,
                                 @NotNull @NotEmpty String text) {
 }
